9ebb25117fd525e8fa28969bbed6df56ec1454db
Ingvar Derešivski <inde999@gmail.com>
2024-02-05 12:29:36 +0200

    setup

--- TARpe22MauiPlanets/TARpe22MauiPlanets/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TARpe22MauiPlanets;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
---
+++ TARpe22MauiPlanets/TARpe22MauiPlanets/Views/StartPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace TARpe22MauiPlanets.Views;
+
+public partial class StartPage : ContentPage
+{
+	public StartPage()
+	{
+		InitializeComponent();
+	}
+}

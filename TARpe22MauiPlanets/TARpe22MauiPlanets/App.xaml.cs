195a308d68ed284bacc7277d227543f2214f87e0
Ingvar Derešivski <inde999@gmail.com>
2024-02-05 12:02:07 +0200

    init

---
+++ TARpe22MauiPlanets/TARpe22MauiPlanets/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TARpe22MauiPlanets;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ TARpe22MauiPlanets/TARpe22MauiPlanets/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TARpe22MauiPlanets;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}

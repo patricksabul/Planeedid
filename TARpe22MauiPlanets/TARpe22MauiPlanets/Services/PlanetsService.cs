a92c09d5095cb569b70dbdb22fe9dae544f18ab0
Ingvar Derešivski <inde999@gmail.com>
2024-02-06 13:15:27 +0200

    StartPage

---
+++ TARpe22MauiPlanets/TARpe22MauiPlanets/Services/PlanetsService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TARpe22MauiPlanets.Models;
+
+namespace TARpe22MauiPlanets.Services
+{
+    internal static class PlanetsService
+    {
+        private static List<Planet> planets = new()
+        {
+            new()
+            {
+
+            }
+        };
+    }
+}

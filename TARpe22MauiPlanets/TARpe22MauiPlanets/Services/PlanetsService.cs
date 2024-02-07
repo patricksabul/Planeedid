2856a4ba848c6dc921a143cfae48937df324729b
Ingvar Derešivski <inde999@gmail.com>
2024-02-07 11:58:24 +0200

    PlanetsInfo

--- TARpe22MauiPlanets/TARpe22MauiPlanets/Services/PlanetsService.cs
+++ TARpe22MauiPlanets/TARpe22MauiPlanets/Services/PlanetsService.cs
@@ -13,8 +13,26 @@ namespace TARpe22MauiPlanets.Services
         {
             new()
             {
-
-            }
+                Name = "Mercury",
+                Subtitle = "The smallest planet",
+                HeroImage = "mercury.png",
+                Description = "Mercury is the smallest planet in the Solar " +
+                "System and the closest to the Sun. Its orbit around the Sun takes " +
+                "87.97 Earth days, the shortest of all the Sun's planets. " +
+                "It is named after the Roman god Mercurius (Mercury), god " +
+                "of commerce, messenger of the gods, and mediator between gods " +
+                "and mortals, corresponding to the Greek god Hermes (Ἑρμῆς). Like " +
+                "Venus, Mercury orbits the Sun within Earth's orbit as an inferior " +
+                "planet; its apparent distance from the Sun as viewed from Earth " +
+                "never exceeds 28°.",
+                AccentColorStart = Color.FromArgb("#353535"),
+                AccentColorEnd = Color.FromArgb("#8d9098"),
+                Images = new() 
+                {
+                    //Otsida kõikide planeetide kohta iseloomulik kirjeldus ja
+                    // täita muud andmed ära
+                }
+            },
         };
     }
 }
